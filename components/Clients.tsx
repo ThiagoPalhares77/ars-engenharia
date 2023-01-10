344f032f4aed7bea89e87795983b531985e6632f
Thiago <ps-thiago@hotmail.com>
2023-01-10 19:59:11 -0300

    Feat Clients images

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 import { motion, Variants } from 'framer-motion'
-import LogoExemple from '/public/ClientsLogo/logoexemple.png'
+import Client1 from '/public/ClientsLogo/apia.jpg'
+import Client2 from '/public/ClientsLogo/GERAÇÕES.png'
+import Client3 from '/public/ClientsLogo/LDC.png'
+import Client4 from '/public/ClientsLogo/OFI.png'
+import Client5 from '/public/ClientsLogo/RUOCCO.png'
+import Client6 from '/public/ClientsLogo/SICOOB.png'
+import Client7 from '/public/ClientsLogo/VINA.png'
 
 export const Clients = () => {
   const cardVariants: Variants = {
@@ -29,26 +35,29 @@ export const Clients = () => {
         Alguns Clientes:
       </h1>
       <motion.ul
-        className="grid md:grid-cols-6 sm:grid-cols-3 grid-cols-2 mx-10"
+        className="grid md:grid-cols-7 sm:grid-cols-3 grid-cols-2 mx-10 gap-2"
         variants={cardVariants}
       >
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client1} alt={''} />
         </li>
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client2} alt={''} />
         </li>
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client3} alt={''} />
         </li>
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client7} alt={''} />
         </li>
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client5} alt={''} />
         </li>
         <li>
-          <Image src={LogoExemple} alt={''} />
+          <Image src={Client6} alt={''} />
+        </li>
+        <li>
+          <Image src={Client4} alt={''} width={120} height={120} />
         </li>
       </motion.ul>
     </motion.div>
